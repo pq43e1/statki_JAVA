4111aec7b7ec1081aeb69f1ee6b056e2bb63c393
pq43e1 <76269570+pq43e1@users.noreply.github.com>
2021-12-01 19:27:16 +0100

    Update Utworz.java

--- Utworz.java
+++ Utworz.java
@@ -2,10 +2,21 @@ import java.util.Scanner;
 
 public class Utworz {
 
-
+    private int numerGracza = 1;
+    private String imieGracza;
+    private char wybor;
+    
+    Scanner sc = new Scanner(System.in);
 
     public void gracza() {
-        System.out.println("Wprowadz imie gracza numer");
+        while(true){
+            System.out.println("Wprowadz imie gracza numer "+numerGracza+":");
+            imieGracza = sc.nextLine();
+            System.out.println("Wybrales imie "+imieGracza+". Czy potwierdzic wybór? T/N");
+            wybor = sc.nextChar();
+            if(wybor == 'T' || wybor == 't')
+                Gracz g1 = new Gracz();
+        }
     }
 
 

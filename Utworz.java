6e4ae1e870389a5002cc25fbbe123b0aea24dfee
pq43e1 <76269570+pq43e1@users.noreply.github.com>
2021-12-01 19:55:17 +0100

    Update Utworz.java

--- Utworz.java
+++ Utworz.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Date;
 
 public class Utworz {
 
@@ -7,6 +8,9 @@ public class Utworz {
     private char wybor;
     private Boolean czyCzlowiek = null;
     
+    Date aktualnaData = new Date();
+    private long timestamp = aktualnaData.getTime();
+    
     Scanner sc = new Scanner(System.in);
 
     public void gracza() {
@@ -14,7 +18,7 @@ public class Utworz {
             System.out.println("Wprowadz imie gracza numer "+numerGracza+":");
             imieGracza = sc.nextLine();
             while(true){
-                System.out.println("Gracz ma być człowiekiem czy komputerem? Dokonaj wyboru wpisujac c lub k:");
+                System.out.println("Gracz ma być człowiekiem czy komputerem? C/K");
                 wybor = sc.nextLine;
                 if(wybor == 'c' || wybor == 'C'){
                     czyCzlowiek = true;
@@ -25,12 +29,27 @@ public class Utworz {
                 }else
                     System.out.println("Wprowadz literke c lub k by kontynowac.");
             }
-            System.out.println("Wybrales imie "+imieGracza+". Gracz będzie sterowany przez "); 
-            System.out.print(czyCzlowiek == true? "czlowieka" : "komputer");
-            System.out.print(". Czy potwierdzasz wybor? T/N");          
+            while(true){
+                System.out.println("Wybrales imie "+imieGracza+". Gracz będzie sterowany przez "); 
+                System.out.print(czyCzlowiek == true? "czlowieka" : "komputer");
+                System.out.print(". Czy potwierdzasz wybor? T/N");          
                 wybor = sc.nextChar();
-                if(wybor == 'T' || wybor == 't')
-                    Gracz g1 = new Gracz(numerGracza, imieGracza, );
+                    if(wybor == 'T' || wybor == 't'){
+                        break;
+                        break;
+                    }else if(wybor == 'N' || wybor == 'n'){
+                        break;
+                    }else{
+                    System.out.println("Wprowadz literke T lub N by kontynowac.");
+                    }      
+            }
+        }
+        if(numerGracza == 1){
+            Gracz gracz1 = new Gracz(numerGracza, imieGracza, czyCzlowiek, timestamp);
+            numerGracza++;
+            wybor = ' ';
+        }else{
+            Gracz gracz2 = new Gracz(numerGracza, imieGracza, czyCzlowiek, timestamp);  
         }
     }
 

4f286b2cfbb469818ea5c2d1ac31ec463606304e
pq43e1 <76269570+pq43e1@users.noreply.github.com>
2021-12-01 19:39:13 +0100

    Update Utworz.java

--- Utworz.java
+++ Utworz.java
@@ -5,6 +5,7 @@ public class Utworz {
     private int numerGracza = 1;
     private String imieGracza;
     private char wybor;
+    private Boolean czyCzlowiek = null;
     
     Scanner sc = new Scanner(System.in);
 
@@ -12,13 +13,26 @@ public class Utworz {
         while(true){
             System.out.println("Wprowadz imie gracza numer "+numerGracza+":");
             imieGracza = sc.nextLine();
-            System.out.println("Wybrales imie "+imieGracza+". Czy potwierdzic wybór? T/N");
-            wybor = sc.nextChar();
-            if(wybor == 'T' || wybor == 't')
-                Gracz g1 = new Gracz();
+            while(true){
+                System.out.println("Gracz ma być człowiekiem czy komputerem? Dokonaj wyboru wpisujac c lub k:");
+                wybor = sc.nextLine;
+                if(wybor == 'c' || wybor == 'C'){
+                    czyCzlowiek = true;
+                    break;
+                }else if (wybor == 'k' || wybor == 'K'){
+                    czyCzlowiek = false;
+                    break;
+                }else
+                    System.out.println("Wprowadz literke c lub k by kontynowac.");
+            }
+            System.out.println("Wybrales imie "+imieGracza+". Gracz będzie sterowany przez "); 
+            System.out.print(czyCzlowiek == true? "czlowieka" : "komputer");
+            System.out.print(". Czy potwierdzasz wybor? T/N");          
+                wybor = sc.nextChar();
+                if(wybor == 'T' || wybor == 't')
+                    Gracz g1 = new Gracz(numerGracza, imieGracza, );
         }
     }
-
 
 
 }

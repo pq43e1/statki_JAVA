f67477ec07ba5da12e3a9fcf58ce257dfac17c7a
pq43e1 <76269570+pq43e1@users.noreply.github.com>
2021-12-05 01:00:20 +0100

    Update Plansza.java

--- Plansza.java
+++ Plansza.java
@@ -1,47 +1,20 @@
+package statki;
+
 public class Plansza {
 
     private int numerGracza;
-    private String imieGracza;
-    private String rodzajPlanszy;
-    private final char oznaczenieRodzaju; // K - kolizje, W - widoczna, U - ukryta
+    private final String imieGracza;
+    private String rodzajPlanszy; // K - kolizje, W - widoczna, U - ukryta
     private char [][] plansza;
 
     public Plansza(String imieGracza, int numerGracza, char oznaczenieRodzaju){
         this.numerGracza = numerGracza;
-        this.oznaczenieRodzaju = oznaczenieRodzaju;
         this.imieGracza = imieGracza;
-        plansza = utworz();
-    }
-
-    public char[][] utworz() {
         if(oznaczenieRodzaju == 'W') rodzajPlanszy = "Plansza widoczna dla gracza.";
         else if(oznaczenieRodzaju == 'U') rodzajPlanszy = "Plansza gracza widoczna dla przeciwnika.";
         else if(oznaczenieRodzaju == 'K') rodzajPlanszy = "Plansza ukryta przed graczem - kolizje.";
         else rodzajPlanszy ="BLAD, NIEZIDENTYFIKOWANA";
-
-        char[][]  plansza = new char[11][21];
-        char iteratorChara = 'A';
-        char iretarorLiczbowu = '1';
-        for (int i = 0; i < 11; i++) {
-            for (int j = 0; j < 20; j++) {
-                if (i == 0 && j == 0) plansza[i][j] = ' ';
-                else if (i == 0 && j > 0) {
-
-                    plansza[i][j] = iteratorChara++;
-                    plansza[i][j+1] = ' ';
-                    j += 1;
-                } else if (i > 0 && j == 0) {
-                    if(i!=10)
-                        plansza[i][j] = iretarorLiczbowu++;
-                }
-                else {
-                    plansza[i][j] = ' ';
-                    plansza[i][j+1] = ' ';
-                    j += 1;
-                }
-            }
-        }
-        return plansza;
+        plansza = Utworz.plansze();
     }
 
     public String getRodzajPlanszy() {
@@ -68,8 +41,8 @@ public class Plansza {
         this.rodzajPlanszy = rodzajPlanszy;
     }
 
-    public void setPlansza(char[][] plansza) {
-        this.plansza = plansza;
+    public void setPlansza(int x, int y, char ch) {
+        plansza[x][y] = ch;
     }
 
 

48d7b69e7ef035530248ef85c57476e5bd621fce
pq43e1 <76269570+pq43e1@users.noreply.github.com>
2021-12-05 01:00:48 +0100

    Update Statek.java

--- Statek.java
+++ Statek.java
@@ -1,13 +1,17 @@
+package statki;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class Statek{
-    
+
     Map<Character, Character> zajmowanePola = new HashMap<>();
     List<String> lista = new ArrayList<>();
-    private int numerGracza;
-    private int numerStatku;
-    private int liczbaMasztow;
+    private final int numerGracza;
+    private final int numerStatku;
+    private final int liczbaMasztow;
 
     public Statek(int numerGracza, int numerStatku, int liczbaMasztow, Map<Character, Character> zajmowanePola){
         this.numerGracza = numerGracza;
@@ -15,21 +19,21 @@ class Statek{
         this.numerStatku = numerStatku;
         this.zajmowanePola = zajmowanePola;
     }
-    
+
     public int getNumerGracza(){
         return numerGracza;
     }
-    
+
     public int getNumerStatku(){
         return numerStatku;
     }
-    
+
     public int getLiczbaMasztow(){
         return liczbaMasztow;
     }
-    
+
     public Map<Character, Character> getZajmowanePola(){
         return zajmowanePola;
     }
-    
+
 }
